7ebe052bfd7bcbd49a9bb6fe821e6af93b8f14da
anilans <anilsai029@gmail.com>
2022-06-02 19:03:11 +0530

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "perceptron-pkg"
+USER_NAME = "anilans029"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="anilsai029@gmail.com",
+    description="A small package for perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/pypa/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/pypa/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy == 1.20.3",
+        "joblib==1.0.1",
+        "pandas==1.3.4",
+        "matplotlib == 3.4.3",
+    ]
+)
